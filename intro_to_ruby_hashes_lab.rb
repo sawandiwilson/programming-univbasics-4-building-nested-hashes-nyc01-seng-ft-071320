54f416094accd072b847e7b43b0147859f505b2c
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-05 23:46:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,8 +35,49 @@ monopoly =  {
   }
 }
     
+<<<<<<< HEAD
+=======
 end
 
+def monopoly_with_fourth_tier
+  monopoly =  {
+  :railroads =>
+  {
+    :pieces =>4,
+    :rent_in_dollars =>
+    {
+      :one_piece_owned =>25,
+      :two_pieces_owned =>50,
+      :three_pieces_owned =>100,
+      :four_pieces_owned => 200
+    },
+    :names =>
+    {
+      :reading_railroad =>
+      {
+        "mortgage_value"=> 100
+      },
+      :pennsylvania_railroad =>
+      {
+        "mortgage_value"=>200
+      },
+      :b_and_o_railroad =>
+      {
+        "mortgage_value"=>400
+      },
+      :shortline =>
+      {
+        "mortgage_value"=>800
+      }
+    }
+  }
+}
+>>>>>>> f5a277a0f621fea8ddbc16b6e26a213f2b6e124a
+end
+  
+    
+
+<<<<<<< HEAD
 def monopoly_with_fourth_tier
   monopoly =  {
   :railroads=>
@@ -65,5 +106,6 @@ def monopoly_with_fourth_tier
     }
   }
 }
+=======
+>>>>>>> f5a277a0f621fea8ddbc16b6e26a213f2b6e124a
 
-end
